94169f1876fcf0e9f79dcc638fa895cc5d8dfc9e
squareGTS <squareGTS@icloud.com>
2021-05-16 20:48:30 +0300

    bug fix 1) Likes 2) User ViewController 3) Add photos 4) add constrains to friends viewController
    add
    1) letter control

---
+++ VK app/Model/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  VK app
+//
+//  Created by Maxim Bekmetov on 16.05.2021.
+//
+
+import UIKit
+
+struct Photo {
+    var photos: UIImage?
+    var like: Bool
+    var count: Int
+}
